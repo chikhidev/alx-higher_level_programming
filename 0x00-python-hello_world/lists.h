d6df1a57ba26eb79fbd2fb4e2d2332098ecd0cd2
Abdoo-ちき <chikhi.dev@gmail.com>
2023-06-07 13:06:04 +0100

    fixing

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -4,17 +4,17 @@
 #include <stdlib.h>
 
 /**
- * struct listint_s - singly linked list
- * @n: integer
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- * 
- */
+* struct listint_s - singly linked list
+* @n: integer
+* @next: points to the next node
+*
+* Description: singly linked list node structure
+* 
+*/
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+int n;
+struct listint_s *next;
 } listint_t;
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
@@ -22,6 +22,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-
 
 #endif /* LISTS_H */
