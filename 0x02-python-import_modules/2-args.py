e607e9b8595587ac7260905c11efbb159502e416
chikhidev <chikhi.dev@gmail.com>
2023-06-08 15:50:50 -0700

    models

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,8 +3,6 @@
 
 if __name__ == "__main__":
     import sys
-    i = 1
-
     count = len(sys.argv) - 1
     if count == 0:
         print("0 arguments.")
@@ -12,7 +10,6 @@ if __name__ == "__main__":
         print("1 argument:")
     else:
         print("{} arguments:".format(count))
-    while i <= count:
-        print("{}: {}".format(i, sys.argv[i]))
-        i += 1
+    for i, arg in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(i, arg))
 
