0e277fbc98565faff349420e68b5797eef9805d5
chikhidev <chikhi.dev@gmail.com>
2023-06-08 15:57:36 -0700

    models

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    argc = len(sys.argv)
+    res = 0
+    for i in range(argc - 1):
+        res += int(sys.argv[i + 1])
+
+    print("{}".format(total))
