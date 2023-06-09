0c2fca67cb95fe5c1afda34cbdb9716af5f3e2b2
chikhidev <chikhi.dev@gmail.com>
2023-06-09 16:24:09 -0700

    models

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,6 +2,6 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list) - 1:
         return my_list
-    cloned_list = my_list
+    cloned_list = my_list[:]
     cloned_list[idx] = element
     return cloned_list
