5c0158dcaac5ff34f90ddf92804be475dfaaf339
chikhidev <chikhi.dev@gmail.com>
2023-06-09 16:31:53 -0700

    models

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,6 +2,6 @@
 def no_c(my_string):
     string = ''
     for char in my_string:
-        if char != ('c' || 'C'):
+        if char != 'c' and char != 'C':
             string += char
     return string
