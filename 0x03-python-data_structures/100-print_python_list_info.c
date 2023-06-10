f2aa80b29799cb7ce222fa8ed8a6d872c9fd7ac8
chikhidev <chikhi.dev@gmail.com>
2023-06-10 15:55:27 -0700

    models

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include <Python.h>
+#include <stdio.h>
+
+void print_python_list_info(PyObject *p)
+{
+    unsigned int len = PyList_Size(p);
+    unsigned int allocated = ((PyListObject *) p)->allocated;
+    unsigned int l_idx = 0;
+
+    if (p == NULL)
+        return;
+
+    printf("[*] Size of the Python List = %u\n", len);
+    printf("[*] Allocated = %u\n", allocated);
+
+    while (l_idx < len)
+    {
+        PyTypeObject *type = PyList_GET_ITEM(p, l_idx)->ob_type;
+        const char *name = type->tp_name;
+        printf("Element %u: %s\n", l_idx, name);
+
+        l_idx++;
+    }
+}
+
