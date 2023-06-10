7667ef1cf17d19d8229408c6c953701d970d7111
chikhidev <chikhi.dev@gmail.com>
2023-06-10 15:28:37 -0700

    models

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,5 @@
 #!/usr/bin/python3
+
 def add_tuple(tuple_a=(), tuple_b=()):
-    res = ()
-    length = max(len(tuple_a), len(tuple_b))
-    i = 0
-    while i <= length:
-        left = tuple_a[i] if i < len(tuple_a) else 0
-        right = tuple_b[i] if i < len(tuple_b) else 0
-        res += (left + right,)
-    return res
+    return tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
 
