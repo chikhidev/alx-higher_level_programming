8c0350258eacc7489a2c633b2e2c0a237e6c9601
chikhidev <chikhi.dev@gmail.com>
2023-06-10 14:50:48 -0700

    models

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > len(my_list) - 1:
-        return my_list
-    cloned_list = my_list[:]
-    cloned_list[idx] = element
-    return cloned_list
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in range(len(row)):
+            print(row[i], end=' ')
+        print()
+
