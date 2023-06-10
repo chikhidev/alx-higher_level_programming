f7ef66f0ddcf815f41f617a010a0de254d268b5f
chikhidev <chikhi.dev@gmail.com>
2023-06-10 14:53:38 -0700

    models

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        for i in range(len(row)):
-            print(row[i], end=' ')
+    row_index = 0
+    while row_index < len(matrix):
+        row = matrix[row_index]
+        elem_index = 0
+        while elem_index < len(row):
+            print('{:d}'.format(row[elem_index]), end=(" " if elem_index < len(row) - 1 else ""))
+            elem_index += 1
         print()
+        row_index += 1
 
