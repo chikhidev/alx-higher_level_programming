41d190050a5beb0474ece0f1b11a35108a83cddd
chikhidev <chikhi.dev@gmail.com>
2023-06-14 16:58:10 -0700

    models

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,15 +2,15 @@
 def roman_to_int(roman_string):
     val = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     res = 0
-    p = 0
-    c = len(roman_string) - 1
+    prev_val = 0
 
-    if type(roman_string) is str and roman_string:
-        while c >= 0:
-            if val[roman_string[c]] >= p:
-                res += val[roman_string[c]]
+    if isinstance(roman_string, str) and roman_string:
+        for c in reversed(roman_string):
+            current_val = val.get(c, 0)
+            if current_val >= prev_val:
+                res += current_val
             else:
-                res -= val[roman_string[c]]
-            p = val[roman_string[c]]
-            c -= 1
+                res -= current_val
+            prev_val = current_val
+    
     return res
