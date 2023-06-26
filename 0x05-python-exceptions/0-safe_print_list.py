ec9a885dc35788e54b9c75aa97cbef015abf9a18
chikhidev <chikhi.dev@gmail.com>
2023-06-26 13:06:08 -0700

    models

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             i += 1
             x -= 1
-        except:
+        except IndexError:
             break
     print()
     return (i)
