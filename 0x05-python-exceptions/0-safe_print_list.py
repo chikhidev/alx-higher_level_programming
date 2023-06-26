c11797ed6128191b31e7bfc51d1e655e4846f39b
chikhidev <chikhi.dev@gmail.com>
2023-06-26 12:59:03 -0700

    models

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    org = x
-    while x >= 0:
+    i = 0
+    while x > 0:
         try:
-            print("{}".format(my_list[x]))
+            print("{}".format(my_list[i]), end="")
+            i+=1
             x-=1
         except:
             break
-    print("\n")
-    return (org - x)
+    print("")
+    return (i)
