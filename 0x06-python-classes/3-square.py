6433c1cea88fd25c33dd0abe2cdac093bea88229
chikhidev <chikhi.dev@gmail.com>
2023-06-27 06:03:13 -0700

    models

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""Square class"""
+
+
+class Square:
+    """Squaare inside"""
+
+    def __init__(self, size=0):
+        """init"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """returns the current square area."""
+        return(self.__size * self.__size)
